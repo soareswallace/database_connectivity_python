95c6deb52b12e0bd218e65668d0f2043d5780ea0
Wallace Soares <soareswallace@icloud.com>
2019-10-31 09:53:49 -0300

    remove class folder

--- classes/empregado.py
+++
@@ -1,7 +0,0 @@
-class Empregado:
-    def __init__(self, cpf, s_cpf, cep, numero, salario):
-        self.cpf = cpf
-        self.s_cpf = s_cpf
-        self.cep = cep
-        self.numero = numero
-        self.salario = salario
