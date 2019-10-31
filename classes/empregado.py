a7c813f64e9d7d69c271f07540f97c424f09351b
Wallace Soares <soareswallace@icloud.com>
2019-10-31 08:56:53 -0300

    Menu de selecao de tabela

---
+++ classes/empregado.py
@@ -0,0 +1,7 @@
+class Empregado:
+    def __init__(self, cpf, s_cpf, cep, numero, salario):
+        self.cpf = cpf
+        self.s_cpf = s_cpf
+        self.cep = cep
+        self.numero = numero
+        self.salario = salario
